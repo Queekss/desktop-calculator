c76add01510fe071f0482a32c37dca7ff0efec92
Queekss <151211348+Queekss@users.noreply.github.com>
2024-02-26 09:14:31 +0000

    Uploaded all files within repo

---
+++ FinalCalc/Form1.Designer.cs
@@ -0,0 +1,420 @@
+﻿namespace FinalCalc
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            components = new System.ComponentModel.Container();
+            resultBox = new TextBox();
+            button1 = new Button();
+            button2 = new Button();
+            button3 = new Button();
+            button4 = new Button();
+            button5 = new Button();
+            button6 = new Button();
+            button7 = new Button();
+            button8 = new Button();
+            button9 = new Button();
+            button0 = new Button();
+            buttonOpenBracket = new Button();
+            buttonCloseBracket = new Button();
+            buttonEquals = new Button();
+            button10 = new Button();
+            buttonAdd = new Button();
+            buttonDivide = new Button();
+            button11 = new Button();
+            buttonPercent = new Button();
+            buttonSqrt = new Button();
+            buttonPower = new Button();
+            buttonUndo = new Button();
+            buttonClear = new Button();
+            buttonSave = new Button();
+            buttonRetrieve = new Button();
+            buttonDecimal = new Button();
+            toolTip1 = new ToolTip(components);
+            SuspendLayout();
+            // 
+            // resultBox
+            // 
+            resultBox.Font = new Font("Segoe UI", 13F);
+            resultBox.Location = new Point(28, 34);
+            resultBox.Multiline = true;
+            resultBox.Name = "resultBox";
+            resultBox.Size = new Size(584, 98);
+            resultBox.TabIndex = 0;
+            resultBox.KeyPress += resultBox_KeyPress;
+            // 
+            // button1
+            // 
+            button1.Font = new Font("Segoe UI", 15F);
+            button1.Location = new Point(28, 251);
+            button1.Name = "button1";
+            button1.Size = new Size(112, 96);
+            button1.TabIndex = 1;
+            button1.Text = "1";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
+            // 
+            // button2
+            // 
+            button2.Font = new Font("Segoe UI", 15F);
+            button2.Location = new Point(146, 251);
+            button2.Name = "button2";
+            button2.Size = new Size(112, 96);
+            button2.TabIndex = 2;
+            button2.Text = "2";
+            button2.UseVisualStyleBackColor = true;
+            button2.Click += button1_Click;
+            // 
+            // button3
+            // 
+            button3.Font = new Font("Segoe UI", 15F);
+            button3.Location = new Point(264, 251);
+            button3.Name = "button3";
+            button3.Size = new Size(112, 96);
+            button3.TabIndex = 3;
+            button3.Text = "3";
+            button3.UseVisualStyleBackColor = true;
+            button3.Click += button1_Click;
+            // 
+            // button4
+            // 
+            button4.Font = new Font("Segoe UI", 15F);
+            button4.Location = new Point(28, 353);
+            button4.Name = "button4";
+            button4.Size = new Size(112, 96);
+            button4.TabIndex = 4;
+            button4.Text = "4";
+            button4.UseVisualStyleBackColor = true;
+            button4.Click += button1_Click;
+            // 
+            // button5
+            // 
+            button5.Font = new Font("Segoe UI", 15F);
+            button5.Location = new Point(146, 353);
+            button5.Name = "button5";
+            button5.Size = new Size(112, 96);
+            button5.TabIndex = 5;
+            button5.Text = "5";
+            button5.UseVisualStyleBackColor = true;
+            button5.Click += button1_Click;
+            // 
+            // button6
+            // 
+            button6.Font = new Font("Segoe UI", 15F);
+            button6.Location = new Point(264, 353);
+            button6.Name = "button6";
+            button6.Size = new Size(112, 96);
+            button6.TabIndex = 6;
+            button6.Text = "6";
+            button6.UseVisualStyleBackColor = true;
+            button6.Click += button1_Click;
+            // 
+            // button7
+            // 
+            button7.Font = new Font("Segoe UI", 15F);
+            button7.Location = new Point(28, 455);
+            button7.Name = "button7";
+            button7.Size = new Size(112, 96);
+            button7.TabIndex = 7;
+            button7.Text = "7";
+            button7.UseVisualStyleBackColor = true;
+            button7.Click += button1_Click;
+            // 
+            // button8
+            // 
+            button8.Font = new Font("Segoe UI", 15F);
+            button8.Location = new Point(146, 455);
+            button8.Name = "button8";
+            button8.Size = new Size(112, 96);
+            button8.TabIndex = 8;
+            button8.Text = "8";
+            button8.UseVisualStyleBackColor = true;
+            button8.Click += button1_Click;
+            // 
+            // button9
+            // 
+            button9.Font = new Font("Segoe UI", 15F);
+            button9.Location = new Point(264, 455);
+            button9.Name = "button9";
+            button9.Size = new Size(112, 96);
+            button9.TabIndex = 9;
+            button9.Text = "9";
+            button9.UseVisualStyleBackColor = true;
+            button9.Click += button1_Click;
+            // 
+            // button0
+            // 
+            button0.Font = new Font("Segoe UI", 15F);
+            button0.Location = new Point(382, 455);
+            button0.Name = "button0";
+            button0.Size = new Size(112, 96);
+            button0.TabIndex = 10;
+            button0.Text = "0";
+            button0.UseVisualStyleBackColor = true;
+            button0.Click += button1_Click;
+            // 
+            // buttonOpenBracket
+            // 
+            buttonOpenBracket.Font = new Font("Segoe UI", 15F);
+            buttonOpenBracket.Location = new Point(28, 149);
+            buttonOpenBracket.Name = "buttonOpenBracket";
+            buttonOpenBracket.Size = new Size(112, 96);
+            buttonOpenBracket.TabIndex = 11;
+            buttonOpenBracket.Text = "(";
+            buttonOpenBracket.UseVisualStyleBackColor = true;
+            buttonOpenBracket.Click += button1_Click;
+            // 
+            // buttonCloseBracket
+            // 
+            buttonCloseBracket.Font = new Font("Segoe UI", 15F);
+            buttonCloseBracket.Location = new Point(146, 149);
+            buttonCloseBracket.Name = "buttonCloseBracket";
+            buttonCloseBracket.Size = new Size(112, 96);
+            buttonCloseBracket.TabIndex = 12;
+            buttonCloseBracket.Text = ")";
+            buttonCloseBracket.UseVisualStyleBackColor = true;
+            buttonCloseBracket.Click += button1_Click;
+            // 
+            // buttonEquals
+            // 
+            buttonEquals.Font = new Font("Segoe UI", 15F);
+            buttonEquals.Location = new Point(500, 557);
+            buttonEquals.Name = "buttonEquals";
+            buttonEquals.Size = new Size(112, 96);
+            buttonEquals.TabIndex = 13;
+            buttonEquals.Text = "=";
+            buttonEquals.UseVisualStyleBackColor = true;
+            buttonEquals.Click += buttonEquals_Click;
+            // 
+            // button10
+            // 
+            button10.Font = new Font("Segoe UI", 15F);
+            button10.Location = new Point(500, 353);
+            button10.Name = "button10";
+            button10.Size = new Size(112, 96);
+            button10.TabIndex = 14;
+            button10.Text = "-";
+            button10.UseVisualStyleBackColor = true;
+            button10.Click += button1_Click;
+            // 
+            // buttonAdd
+            // 
+            buttonAdd.Font = new Font("Segoe UI", 15F);
+            buttonAdd.Location = new Point(382, 353);
+            buttonAdd.Name = "buttonAdd";
+            buttonAdd.Size = new Size(112, 96);
+            buttonAdd.TabIndex = 15;
+            buttonAdd.Text = "+";
+            buttonAdd.UseVisualStyleBackColor = true;
+            buttonAdd.Click += button1_Click;
+            // 
+            // buttonDivide
+            // 
+            buttonDivide.Font = new Font("Segoe UI", 15F);
+            buttonDivide.Location = new Point(382, 251);
+            buttonDivide.Name = "buttonDivide";
+            buttonDivide.Size = new Size(112, 96);
+            buttonDivide.TabIndex = 16;
+            buttonDivide.Text = "/";
+            buttonDivide.UseVisualStyleBackColor = true;
+            buttonDivide.Click += button1_Click;
+            // 
+            // button11
+            // 
+            button11.Font = new Font("Segoe UI", 15F);
+            button11.Location = new Point(382, 149);
+            button11.Name = "button11";
+            button11.Size = new Size(112, 96);
+            button11.TabIndex = 17;
+            button11.Text = "*";
+            button11.UseVisualStyleBackColor = true;
+            button11.Click += button1_Click;
+            // 
+            // buttonPercent
+            // 
+            buttonPercent.Font = new Font("Segoe UI", 15F);
+            buttonPercent.Location = new Point(264, 149);
+            buttonPercent.Name = "buttonPercent";
+            buttonPercent.Size = new Size(112, 96);
+            buttonPercent.TabIndex = 18;
+            buttonPercent.Text = "%";
+            buttonPercent.UseVisualStyleBackColor = true;
+            buttonPercent.Click += buttonPercent_Click;
+            // 
+            // buttonSqrt
+            // 
+            buttonSqrt.Font = new Font("Segoe UI", 15F);
+            buttonSqrt.Location = new Point(500, 455);
+            buttonSqrt.Name = "buttonSqrt";
+            buttonSqrt.Size = new Size(112, 96);
+            buttonSqrt.TabIndex = 19;
+            buttonSqrt.Text = "√";
+            buttonSqrt.UseVisualStyleBackColor = true;
+            buttonSqrt.Click += buttonPercent_Click;
+            // 
+            // buttonPower
+            // 
+            buttonPower.Font = new Font("Segoe UI", 15F);
+            buttonPower.Location = new Point(382, 557);
+            buttonPower.Name = "buttonPower";
+            buttonPower.Size = new Size(112, 96);
+            buttonPower.TabIndex = 20;
+            buttonPower.Text = "^";
+            buttonPower.UseVisualStyleBackColor = true;
+            buttonPower.Click += button1_Click;
+            // 
+            // buttonUndo
+            // 
+            buttonUndo.Font = new Font("Segoe UI", 15F);
+            buttonUndo.Location = new Point(500, 251);
+            buttonUndo.Name = "buttonUndo";
+            buttonUndo.Size = new Size(112, 96);
+            buttonUndo.TabIndex = 21;
+            buttonUndo.Text = "C";
+            buttonUndo.UseVisualStyleBackColor = true;
+            buttonUndo.Click += buttonClear_Click;
+            // 
+            // buttonClear
+            // 
+            buttonClear.Font = new Font("Segoe UI", 15F);
+            buttonClear.Location = new Point(500, 149);
+            buttonClear.Name = "buttonClear";
+            buttonClear.Size = new Size(112, 96);
+            buttonClear.TabIndex = 22;
+            buttonClear.Text = "CE";
+            buttonClear.UseVisualStyleBackColor = true;
+            buttonClear.Click += buttonClear_Click;
+            // 
+            // buttonSave
+            // 
+            buttonSave.Font = new Font("Segoe UI", 11F);
+            buttonSave.Location = new Point(28, 557);
+            buttonSave.Name = "buttonSave";
+            buttonSave.Size = new Size(112, 96);
+            buttonSave.TabIndex = 23;
+            buttonSave.Text = "SAVE";
+            buttonSave.UseVisualStyleBackColor = true;
+            buttonSave.Click += buttonSave_Click;
+            // 
+            // buttonRetrieve
+            // 
+            buttonRetrieve.Font = new Font("Segoe UI", 11F);
+            buttonRetrieve.Location = new Point(146, 557);
+            buttonRetrieve.Name = "buttonRetrieve";
+            buttonRetrieve.Size = new Size(112, 96);
+            buttonRetrieve.TabIndex = 24;
+            buttonRetrieve.Text = "RETRIEVE";
+            toolTip1.SetToolTip(buttonRetrieve, "Enter your equation in the text box, and press RETRIEVE to check it exists");
+            buttonRetrieve.UseVisualStyleBackColor = true;
+            buttonRetrieve.Click += buttonRetrieve_Click;
+            // 
+            // buttonDecimal
+            // 
+            buttonDecimal.Font = new Font("Segoe UI", 15F);
+            buttonDecimal.Location = new Point(264, 557);
+            buttonDecimal.Name = "buttonDecimal";
+            buttonDecimal.Size = new Size(112, 96);
+            buttonDecimal.TabIndex = 25;
+            buttonDecimal.Text = ".";
+            buttonDecimal.UseVisualStyleBackColor = true;
+            buttonDecimal.Click += button1_Click;
+            // 
+            // toolTip1
+            // 
+            toolTip1.AutoPopDelay = 5000;
+            toolTip1.InitialDelay = 300;
+            toolTip1.ReshowDelay = 100;
+            toolTip1.ToolTipIcon = ToolTipIcon.Info;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(10F, 25F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(644, 674);
+            Controls.Add(buttonDecimal);
+            Controls.Add(buttonRetrieve);
+            Controls.Add(buttonSave);
+            Controls.Add(buttonClear);
+            Controls.Add(buttonUndo);
+            Controls.Add(buttonPower);
+            Controls.Add(buttonSqrt);
+            Controls.Add(buttonPercent);
+            Controls.Add(button11);
+            Controls.Add(buttonDivide);
+            Controls.Add(buttonAdd);
+            Controls.Add(button10);
+            Controls.Add(buttonEquals);
+            Controls.Add(buttonCloseBracket);
+            Controls.Add(buttonOpenBracket);
+            Controls.Add(button0);
+            Controls.Add(button9);
+            Controls.Add(button8);
+            Controls.Add(button7);
+            Controls.Add(button6);
+            Controls.Add(button5);
+            Controls.Add(button4);
+            Controls.Add(button3);
+            Controls.Add(button2);
+            Controls.Add(button1);
+            Controls.Add(resultBox);
+            Name = "Form1";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private TextBox resultBox;
+        private Button button1;
+        private Button button2;
+        private Button button3;
+        private Button button4;
+        private Button button5;
+        private Button button6;
+        private Button button7;
+        private Button button8;
+        private Button button9;
+        private Button button0;
+        private Button buttonOpenBracket;
+        private Button buttonCloseBracket;
+        private Button buttonEquals;
+        private Button button10;
+        private Button buttonAdd;
+        private Button buttonDivide;
+        private Button button11;
+        private Button buttonPercent;
+        private Button buttonSqrt;
+        private Button buttonPower;
+        private Button buttonUndo;
+        private Button buttonClear;
+        private Button buttonSave;
+        private Button buttonRetrieve;
+        private Button buttonDecimal;
+        private ToolTip toolTip1;
+    }
+}
---
+++ FinalCalc/Form1.cs
@@ -0,0 +1,227 @@
+﻿namespace FinalCalc;
+using org.matheval;
+using System.Text.RegularExpressions;
+using System.IO;
+
+
+public partial class Form1 : Form
+{
+    public Form1()
+    {
+        InitializeComponent();
+    }
+
+
+    // Global variable to store calculation under the hood
+    public string calculation = "";
+
+
+
+    //Method to sanitise inputs prior to saving/calculating
+    public string Sanitise(string input)
+    {
+        input = Regex.Replace(input, @"[^0-9+\-*/%√^().]", "");
+        return input;
+    }
+
+    //Event handler to prevent users from using keyboard input
+    private void resultBox_KeyPress(object sender, KeyPressEventArgs e)
+    {
+        e.Handled = true;
+    }
+
+
+    //Shared event handler for controls that do not require "translation" i.e +
+    private void button1_Click(object sender, EventArgs e)
+    {
+        Button button = sender as Button;
+        resultBox.Text += button.Text;
+        calculation += button.Text;
+    }
+    //Shared event handler for controls that require "translation"
+    private void buttonPercent_Click(object sender, EventArgs e)
+    {
+        Button button = sender as Button;
+
+        switch (button.Text)
+        {
+            case "√":
+                resultBox.Text += button.Text + "(";
+                calculation += "sqrt(";
+                break;
+            case "%": // Regex to capture the number prior to the selection of %, and convert to decimal equivalent
+                resultBox.Text = Sanitise(resultBox.Text);
+                Regex regex = new Regex(@".*?([0-9]+)$");
+                Match match = regex.Match(resultBox.Text);
+                if (match.Success)
+                {
+                    string matchingNumber = match.Groups[1].Value;
+                    string newDecimal = (System.Convert.ToDecimal(matchingNumber) / 100).ToString();
+                    resultBox.Text = resultBox.Text.Remove(resultBox.Text.Length - matchingNumber.Length);
+                    calculation = calculation.Remove(calculation.Length - matchingNumber.Length);
+                    resultBox.Text += newDecimal;
+                    calculation += newDecimal;
+                }
+                break;
+
+
+        }
+
+    }
+
+    //Event handler to calculate expression
+    private void buttonEquals_Click(object sender, EventArgs e)
+    {
+        calculation = Sanitise(calculation);
+       // MessageBox.Show(calculation);
+
+
+        //Check for empty calculation
+
+        if (String.IsNullOrEmpty(calculation))
+        {
+            MessageBox.Show("No calculation has been inputted.", "Empty Calculation", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+            return;
+        }
+
+        try
+        {   //Expression evaluation, using org.matheval library
+            Expression expression = new Expression(calculation);
+            Object result = expression.Eval();
+            resultBox.Clear();
+            resultBox.Text = result.ToString();
+            calculation += $"={resultBox.Text}";
+        }
+        catch (Exception ex)
+        {
+            MessageBox.Show("An error occurred when trying to calculate expression. Ensure only valid characters are entered.", "Error in Calculation", MessageBoxButtons.OK, MessageBoxIcon.Error);
+
+        }
+
+
+    }
+
+    //Event handler for C  and CE
+    private void buttonClear_Click(object sender, EventArgs e)
+    {
+        Button button = sender as Button;
+        if (button.Text == "C")
+        {
+            resultBox.Clear();
+            calculation = "";
+        }
+        else //Removes last character, as the CE symbol
+        {
+            if(resultBox.Text.Length > 0 && calculation.Length > 0)
+            {
+                //if the last character of the resultBoxText is a sqrt symmvol, remove all sqrt characters from the sqrt variable
+                if (resultBox.Text[resultBox.Text.Length - 1].ToString() == "√")
+                {
+                    //Regex for sqrt
+                    Regex regex = new Regex(@".*?([sqrt]{1,4})$");
+                    Match match = regex.Match(calculation);
+                    //Number of characters within resultBox and calculation differ , so when removign the "√" symbol in resultBox, have to remove "sqrt" in calc
+                    resultBox.Text = resultBox.Text.Remove(resultBox.Text.Length - 1);
+                    calculation = calculation.Remove(calculation.Length - match.Groups[1].Value.Length);
+                }
+                else
+                {
+                    resultBox.Text = resultBox.Text.Remove(resultBox.Text.Length - 1);
+                    calculation = calculation.Remove(calculation.Length - 1);
+                }
+
+            }
+            else //As CE uses indexing to function, this catches the potential error of user clearing outside the defined index; uses if else todeal with more gracefully
+            {
+                MessageBox.Show("Nothing to clear. Do not CE if text box is empty", "CE Info", MessageBoxButtons.OK, MessageBoxIcon.Information);
+
+            }
+
+
+        }
+
+    }
+
+    private void buttonSave_Click(object sender, EventArgs e)
+    {
+
+        string filepath = @"C:\Users\Eunic\hi.txt";
+        calculation = Sanitise(calculation);
+        string entry = $"{calculation} {DateTime.Now}";
+        //Erro handling to catch all IO errors
+        try
+        {
+            File.AppendAllText(filepath, entry + Environment.NewLine);
+            string message = $"Equation saved successfully:\n{calculation}";
+            MessageBox.Show(message, "Save Successful", MessageBoxButtons.OK, MessageBoxIcon.Information); //Shows message in body and an info icon
+        }
+        catch (IOException ex)
+        {
+            MessageBox.Show($"File Error: {ex.Message}", "File Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+        }
+
+
+    }
+
+    private void buttonRetrieve_Click(object sender, EventArgs e)
+    {
+        string filepath = @"C:\Users\hi.txt";
+        string record = String.Empty;
+        //Error handling to catch all IO errors
+        try
+        {
+            if (File.Exists(filepath))
+            {
+                foreach (string line in File.ReadLines(filepath))
+                {
+                    //Error handling; if line not found
+                    if (line.Contains(resultBox.Text + " ")) // space as <calculation> <date>, otherwise calculations sharing numbers may be displayed
+                    {
+                        record = line;
+                        break;
+
+                    }
+                }
+
+                if (!(record == String.Empty))
+                {
+                    MessageBox.Show(record, "Equation Found", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                }
+                else
+                {
+                    MessageBox.Show("Equation not found", "Not Found", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                }
+
+            }
+
+
+            else
+            {
+                MessageBox.Show("Equation not found", "Not Found", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+
+        }
+        catch (IOException ex)
+        {
+            MessageBox.Show($" File Retrieval Error: {ex.Message}", "File Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+        }
+
+
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+    //Event handler for controls that require translation
+
+}
+
+
